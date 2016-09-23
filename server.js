bda17aae3e72e76d69ce8c7d78906afce492bdbd
chaitu89 <chaitu89@gmail.com>
2016-09-23 18:24:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,8 +76,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articles['article-one']));
+app.get('/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/madi.png', function (req, res) {

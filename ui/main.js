b1d713fa66d953e08cca9d9519217380eae94f43
chaitu89 <chaitu89@gmail.com>
2016-09-25 18:58:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 
 // Move image
-
-var img = getElementbyId("chaitu";
+)
+var img = getElementbyId("chaitu");
 img.onclick = function() {
   img.style.marginLeft = "100px"
 };

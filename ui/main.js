38f8fdeb8467eb5a423ffcbcfe8e9281cbb9406c
chaitu89 <chaitu89@gmail.com>
2016-09-25 23:55:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,21 @@ button.onclick = function() {
 	request.open('GET','http://chaitu89.imad.hasura-app.io/counter',true);
 	request.send(null);
 };
+
+// Populate a name list
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+  // Make a request to the server and send the name
+  
+  //Capture a list of names and rent it on a list
+  var names = ['name1','name2','name3','name4'];
+  var list = '';
+  for(var i=0;i<names.length;i++) {
+      list = '<li>'+names[i]+'</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};

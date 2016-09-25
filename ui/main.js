5cf813b31dca2c89149d5f1bce770068ffb39ae0
chaitu89 <chaitu89@gmail.com>
2016-09-26 00:23:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,13 +30,26 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
   // Make a request to the server and send the name
-  
-  //Capture a list of names and rent it on a list
-  var names = ['name1','name2','name3','name4'];
+  	// Creat a request object 
+	var request = new XMLHttpRequest();
+	
+	//Capture the response and store it in a variable
+	request.onreadystatechange = function() {
+		if(request.readyState === XMLHttpRequest.DONE) {
+			if(request.status === 200) {
+		var names = request.responseText;
+		names = JSON.parse(names);
   var list = '';
   for(var i=0;i<names.length;i++) {
       list += '<li>'+names[i]+'</li>';
   }
   var ul = document.getElementById('namelist');
   ul.innerHTML = list;
+		}	
+		}
+  //Capture a list of names and rent it on a list
+	};
+	// Make a request to the counter
+	request.open('GET','http://chaitu89.imad.hasura-app.io/submit-name?name='+name,true);
+	request.send(null);
 };
